9a25bed4be45cc7a0de80542f3815565d65e5cbc
Arvin Atienza <arvin.atienza@gmail.com>
2024-11-04 15:07:22 +0000

    wip: fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -50,23 +50,23 @@ function App() {
       </div>
 
 
-      {/* <h1>My Todo's</h1> */}
-      {/* <button onClick={createTodo}>+ new</button> */}
-      {/* <ul> */}
-      {/*   {todos.map((todo) => ( */}
-      {/*     <li  */}
-      {/*       key={todo.id}  */}
-      {/*       onClick={() => deleteTodo(todo.id)} */}
-      {/*     >{todo.content}</li> */}
-      {/*   ))} */}
-      {/* </ul> */}
-      {/* <div> */}
-      {/*   🥳 App successfully hosted. Try creating a new todo. */}
-      {/*   <br /> */}
-      {/*   <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates"> */}
-      {/*     Review next step of this tutorial. */}
-      {/*   </a> */}
-      {/* </div> */}
+      <h1>My Todo's</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li 
+            key={todo.id} 
+            onClick={() => deleteTodo(todo.id)}
+          >{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
       {/* <button onClick={signOut}>Sign out</button> */}
     </main>
   );
